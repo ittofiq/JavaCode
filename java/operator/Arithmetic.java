376a728c5d06cbcf8040a44d55f1e2f54e0c2baa
Ittofiq Surgawan <ittofiq@yahoo.com>
2018-10-16 22:59:41 +0700

    Add Arithmetic operator

---
+++ java/operator/Arithmetic.java
@@ -0,0 +1,43 @@
+package operator;
+
+/*
+ * Operator 	Description
+ * +			Additive operator (also used for String concatenation
+ * -			Subtraction operator
+ * * 			Multiplication operator
+ * /			Division operator
+ * % 			Remainder operator
+ * 
+ */
+
+public class Arithmetic {
+
+	public static void main(String[] args) {
+	
+		int x = 1 + 2;	// x is now 3
+		System.out.println("1 + 2 = "+ x);
+		int result = x;
+		
+		x = x - 1;	// x is now 2
+		System.out.println(result +" - 1 = "+ x);
+		result = x;
+		
+		x = x * 2;	// x is now 4
+		System.out.println(result +" * 2 = "+ x);
+		result = x;
+		
+		x = x / 2;	// x is now 2
+		System.out.println(result +" / 2 = "+ x);
+		result = x;
+		
+		x = x + 8;	// x is now 10
+		System.out.println(result +" + 8 = "+ x);
+		result = x;
+		
+		x = x % 7;	// x is now 3
+		System.out.println(result +" % 7 = "+ x);
+		
+		//	+ Additive used for String concatenation
+		System.out.println("Thank "+ "you.");
+	}
+}
