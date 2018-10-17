970b22808961660e83f96680a005575d33d82812
ittofiq <ittofiq@yahoo.com>
2018-10-17 17:33:33 +0700

    Add Short Data Type

---
+++ java/primitive/ShortDataType.java
@@ -0,0 +1,24 @@
+package primitive;
+
+/*
+ * 
+ * Short data type is a 16-bit signed two's complement integer
+ * Minimum value is -32,768 (-2^15)
+ * Maximum value is 32,767 (inclusive) (2^15 -1)
+ * Short data type can also be used to save memory as byte data type. 
+ * A short is 2 times smaller than an integer
+ * Default value is 0.
+ * Example: short s = 10000, short r = -20000 
+ * 
+ */
+
+public class ShortDataType {
+	
+	public static void main(String[] args) {
+		
+		short s = 32767;
+		
+		System.out.println(s);
+	}
+
+}
