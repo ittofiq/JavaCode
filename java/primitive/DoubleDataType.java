7f0b7692d4e876db5b4758c6a99c37dcf9b16222
ittofiq <ittofiq@yahoo.com>
2018-10-18 13:37:24 +0700

    Add Double data type

---
+++ java/primitive/DoubleDataType.java
@@ -0,0 +1,19 @@
+package primitive;
+
+ /* 
+  *  double data type is a double-precision 64-bit IEEE 754 floating point
+  *  This data type is generally used as the default data type for decimal values, generally the default choice
+  *  Double data type should never be used for precise values such as currency
+  *  Default value is 0.0d
+  *  Example: double d1 = 123.4
+  *  
+  */
+
+public class DoubleDataType {
+
+	public static void main(String[] args) {
+		
+		double d = 123.12345678901234567890d;
+		System.out.println(d);
+	}
+}
