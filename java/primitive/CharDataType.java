2bdadf67c1ee39c437a89f81c4900d5d03070348
ittofiq <ittofiq@yahoo.com>
2018-10-18 13:37:55 +0700

    Add Char data type

---
+++ java/primitive/CharDataType.java
@@ -0,0 +1,18 @@
+package primitive;
+
+/*
+ *   Char data type is a single 16-bit Unicode character
+ *   Minimum value is '\u0000' (or 0)
+ *   Maximum value is '\uffff' (or 65,535 inclusive)
+ *   Char data type is used to store any character
+ *   Example: char letterA = 'A'
+ *
+ */
+
+public class CharDataType {
+
+	public static void main(String[] args) {
+		char c = 'C';
+		System.out.println(c);
+	}
+}
