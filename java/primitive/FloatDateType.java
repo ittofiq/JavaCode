ea8422b1d039dc8393aea3f3bd93c84053a5b30b
ittofiq <ittofiq@yahoo.com>
2018-10-18 13:37:10 +0700

    Add Float data type

---
+++ java/primitive/FloatDateType.java
@@ -0,0 +1,22 @@
+package primitive;
+
+/*
+ *  Float data type is a single-precision 32-bit IEEE 754 floating point
+ *  Float is mainly used to save memory in large arrays of floating point numbers
+ *  Default value is 0.0f
+ *  Float data type is never used for precise values such as currency
+ *  Example: float f1 = 234.5f
+ *  For that, you will need to use the java.math.BigDecimal class instead. 
+ *  Numbers and Strings covers BigDecimal and other useful classes provided by the Java platform.
+ * 
+ */
+
+public class FloatDateType {
+
+	public static void main(String[] args) {
+		
+		float f = 123.1234567890f;
+		
+		System.out.println(f);
+	}
+}
