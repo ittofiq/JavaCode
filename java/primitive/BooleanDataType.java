e66f2a62989f8ba5d79b32f743070ef0d5b8f414
ittofiq <ittofiq@yahoo.com>
2018-10-18 13:37:44 +0700

    Add Boolean data type

---
+++ java/primitive/BooleanDataType.java
@@ -0,0 +1,19 @@
+package primitive;
+
+/*
+ * 
+ *  boolean data type represents one bit of information
+ *  There are only two possible values: true and false
+ *  This data type is used for simple flags that track true/false conditions
+ *  Default value is false
+ *  Example: boolean one = true
+ * 
+ */
+
+public class BooleanDataType {
+
+	public static void main(String[] args) {
+		boolean b = true;
+		System.out.println(b);
+	}
+}
