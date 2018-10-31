45552ac566aa2bf00d068a8e14a54916dd11ebac
ittofiq <ittofiq@yahoo.com>
2018-10-31 14:07:04 +0700

    Add Logic Statement

--- java/statement/LogicStatement.java
+++ java/statement/LogicStatement.java
@@ -1,11 +1,24 @@
-package logic;
+package statement;
 
-public class Logic {
+/*
+ * Logic operator (>, >=, <, <=, ==, !=)
+ * Logic gate (AND = &&, OR = ||, NOT = !)
+ * 
+ */
+
+public class LogicStatement {
 	
 	public static void main(String[] args) {
 		
-		System.out.println(3>4);
-		System.out.println(3<4);
+		/*
+		 * && (only true and true = TRUE, other FALSE)
+		 * || (only false or false = FALSE, other TRUE)
+		 * ! (only NOT(true) = FALSE, NOT(false) = TRUE)
+		 * 
+		 */
+		System.out.println((3<5) && (5<7));
+		System.out.println((4>=6) || (4<=2));
+		System.out.println(!(2<4));
 	}
 
 }
