e168e66f7defb545bd63bbfccf32f01356b48c37
ittofiq <ittofiq@yahoo.com>
2018-11-02 18:19:22 +0700

    Add For Loop

---
+++ java/loop/ForLoop.java
@@ -0,0 +1,14 @@
+package loop;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+
+		for(int i=1; i<20; i+=1) {
+			// System.out.println("count:"+i);
+			if(i % 4 == 0)
+				System.out.println("mode by 4 :"+i);
+		}
+	}
+
+}
