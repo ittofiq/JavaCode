d22a414d94da181b25129fb6f4a2e0d5a784f9f9
Ittofiq Surgawan <ittofiq@yahoo.com>
2018-11-05 00:43:03 +0700

    Add For nested loop

---
+++ java/loop/ForNestedLoop.java
@@ -0,0 +1,16 @@
+package loop;
+
+public class ForNestedLoop {
+
+	public static void main(String[] args) {
+
+		for(int i=0; i<10; i++) {
+			System.out.println("*");
+			for(int j=i; j>=0; j--) {
+				System.out.print("-");
+			}
+		}
+
+	}
+
+}
