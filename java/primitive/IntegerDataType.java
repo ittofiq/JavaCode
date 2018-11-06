c0e520ab1f10b9b4cdac8eb901ca6bc6f802b3f2
ittofiq <ittofiq@yahoo.com>
2018-11-06 15:10:04 +0700

    Add integer data type

--- java/primitive/IntegerDataType.java
+++ java/primitive/IntegerDataType.java
@@ -3,7 +3,7 @@ package primitive;
 /*
  * 
  * Int data type is a 32-bit signed two's complement integer.
- * Minimum value is - 2,147,483,648 (-2^31)
+ * Minimum value is -2,147,483,648 (-2^31)
  * Maximum value is 2,147,483,647(inclusive) (2^31 -1)
  * Integer is generally used as the default data type 
  * for integral values unless there is a concern about memory.
@@ -16,8 +16,19 @@ public class IntegerDataType {
 
 	public static void main(String[] args) {
 		
-		int i = 1_234_567_890;
+		int minValue = -2147483648;
+		int maxValue = 2_147_483_647;
 		
-		System.out.println(i);
+		int firstValue = 5+3;
+		int secondValue = (2*4) + (4-2);
+		int totalValue = 10 + firstValue + secondValue;
+		
+		int newMinValue = minValue/2;
+		
+		System.out.println(minValue);
+		System.out.println(maxValue);
+		
+		System.out.println(totalValue);
+		System.out.println(newMinValue);
 	}
 }
