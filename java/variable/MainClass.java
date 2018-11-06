0c6b3da5389ee4b604721a9604fffd08ef4197c2
ittofiq <ittofiq@yahoo.com>
2018-11-06 11:01:02 +0700

    Add declare variable

--- java/variable/MainClass.java
+++ java/variable/MainClass.java
@@ -6,6 +6,21 @@ public class MainClass {
 		
 		System.out.println("Hello Java");
 		
+		int firstNumber = (5-2) + (2*4);
+		int secondNumber = 15;
+		int thirdNumber = firstNumber * 3;
+		
+		int totalNumber = firstNumber + secondNumber + thirdNumber;
+		
+		int lastOne = 100 - totalNumber;
+		
+		System.out.println(firstNumber);
+		System.out.println("secondNumber");
+		System.out.println(secondNumber);
+		System.out.println(thirdNumber);
+		
+		System.out.println(totalNumber);
+		System.out.println(lastOne);
 	}
 
 }
