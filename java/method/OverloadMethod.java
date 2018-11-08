a205ee1d7b4b7f2bfb88071a4a0d59c10c8e9e3a
ittofiq <ittofiq@yahoo.com>
2018-11-08 15:51:58 +0700

    Add Overload method

---
+++ java/method/OverloadMethod.java
@@ -0,0 +1,29 @@
+package method;
+
+public class OverloadMethod {
+
+	public static void main(String[] args) {
+		
+		int newScore = calculateScore("Tim", 500);
+		System.out.println("New score is " + newScore);
+		
+		calculateScore(200);
+		calculateScore();
+	}
+	
+	public static int calculateScore(String playerName, int score) {
+		System.out.println("Player " + playerName + " scored " + score + " points");
+		return score * 1000;
+	}
+	
+	public static int calculateScore(int score) {
+		System.out.println("Unnamed player scored " + score + " points");
+		return score * 1000;
+	}
+	
+	public static int calculateScore() {
+		System.out.println("No player name, no player score");
+		return 0;
+	}
+
+}
